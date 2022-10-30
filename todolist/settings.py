7f6effc594d06c8211927f37bd078638a7368edc
pratyush770 <pratyushmajumdar27@gmail.com>
2022-10-30 21:48:18 +0530

    first commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -78,17 +78,10 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ToDoListDB',
-        'USER':'postgres',
-        'PASSWORD':'pratyush',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
