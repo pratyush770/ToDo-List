9f8b72edcae96da82c5da31d59ce7d512c9141a4
pratyush770 <pratyushmajumdar27@gmail.com>
2022-11-02 18:46:46 +0530

    first commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_*ov-%+q3h&u@($l=+ysni*24%(ll=v+gh9#mscz9#d*3v^su0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','todolist-pratyush.herokuapp.com']
 
